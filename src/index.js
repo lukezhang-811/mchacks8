1fe9c929e16025dc9577583b57850ac07d0c695f
lukezhang-811 <luke.lq.zhang@gmail.com>
2021-01-30 22:46:59 -0800

    random

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+// const store = createStore(allReducers)
 
 ReactDOM.render(
   <React.StrictMode>
