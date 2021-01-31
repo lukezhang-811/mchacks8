4699bb71450a33f6678e07eba64554e7e247da64
lukezhang-811 <luke.lq.zhang@gmail.com>
2021-01-30 20:08:44 -0800

    connected axios

--- src/screens/tapscreen.jsx
+++ src/screens/tapscreen.jsx
@@ -4,6 +4,10 @@ import Countdown from 'react-countdown';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 
+import axios from "axios";
+import { API_URL } from "../index.js";
+
+
 class TapScreen extends Component {
 
     constructor(props) {
@@ -25,10 +29,14 @@ class TapScreen extends Component {
     componentWillMount(){
         setTimeout( async() => {
             console.log(this.state.clickTimes);
-            const response = await fetch("http://127.0.0.1:8000/", {
+            /*const response = await fetch("http://127.0.0.1:8000/", {
                 method: 'POST',
                 body: JSON.stringify(this.state.clickTimes)
-            })
+            })*/
+            axios.post(API_URL, this.state.clickTimes).then(() => {
+                this.props.resetState();
+                this.props.toggle();
+            });
             this.props.history.push('/break-default');
         }, 15000)
     }
